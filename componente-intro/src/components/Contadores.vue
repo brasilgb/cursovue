c16bd25318929c5d0a610d22d37cc505cb44245d
Anderson Brasil <andersonbrsil@outlook.com>
2020-12-14 17:27:29 -0300

    Push 14-12-2020 2

---
+++ componente-intro/src/components/Contadores.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="contadores">
+     <app-contador/>
+  </div>
+</template>
+
+<script>
+import Contador from './Contador.vue'
+export default {
+ components: {'app-contador': Contador}
+}
+</script>
+
+<style>
+.contadores span{
+    border-bottom: 1px solid #ccc;
+    height: 30px;
+    border-radius: 15px;
+}
+
+.contadores button{
+    height: 30px;
+    width: 30px;
+    border-radius: 15px;
+    background-color: coral;
+    color: #fff;
+    margin-left: 10px;
+    outline: none;
+}
+</style>
