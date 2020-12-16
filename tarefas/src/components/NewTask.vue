eb6b1a90d166a1ef18ad6837d34246179a2bbd2a
Anderson Brasil <andersonbrsil@outlook.com>
2020-12-16 13:35:19 -0300

    Push 16-12-2020 01

---
+++ tarefas/src/components/NewTask.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <div class="new-task">
+    <input type="text" name="" id="" />
+    <button>+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+    };
+  },
+  methods: {
+    add() {
+        
+    }
+  },
+};
+</script>
+
+<style>
+</style>
